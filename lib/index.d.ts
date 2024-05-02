32875fc1439b25ba8a49a5085cc235254348a3d8
Sandeep Rawat <61680562+sandeep-r-bharatpe@users.noreply.github.com>
2024-05-02 16:32:46 +0530

    moved to private registry (#20)
    * feat(private-registry): moved to private registry
    
    * feat(private-registry): private registry update
    
    * chore(log): delete log files
    
    ---------
    
    Co-authored-by: Sandeep Rawat <>

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './CommonUtils/index';
-export * from './StorageUtils/index';
-export * from './Geolocation/index';
-export * from './CurrencyUtils/index';
-export * from './UrlUtils/index';
